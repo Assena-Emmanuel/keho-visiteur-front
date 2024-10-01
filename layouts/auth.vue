4260f94c3212eab49a6cfb72d49d8e517b5240a5
Assena-Emmanuel <assenaemmanuel3@gmail.com>
2024-10-01 16:45:50 +0000

    Auth V10

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -12,9 +12,9 @@ export default {
       <BContainer>
         <BRow class="d-flex justify-content-center">
           <BCol cols="12">
-            <nuxt-link to="/" class="mb-5 d-block auth-logo">
+            <nuxt-link to="/" class="mb-3 d-block auth-logo">
               <div class="text-center">
-                <img src="/images/logo_keho.jpg" alt="" width="50" height="50" class="rounded" />
+                <img src="/images/logo_keho.jpg" alt="" width="70" height="70" class="rounded" />
               </div>
               <!-- <img src="/images/logo_keho.jpg" alt="" height="100" class="logo logo-dark img-thumbnail" />
               <img src="/images/logo_keho.jpg" alt="" height="100" class="logo logo-light" /> -->
