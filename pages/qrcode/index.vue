22f6dd7e4dad72669f651f19e7eeae8dd487eaa3
Assena-Emmanuel <assenaemmanuel3@gmail.com>
2024-10-16 17:54:23 +0000

    qrcode reader v3

--- pages/qrcode/index.vue
+++ pages/qrcode/index.vue
@@ -1,16 +1,13 @@
 <template>
-  <div class="qr-container">
-    <div class="camera-box">
-      <QrcodeStream @decode="onDecode" @init="onInit" />
-    </div>
+  <div>
+    <QrcodeStream @decode="onDecode" @init="onInit" />
+    <!-- Autres composants QR code si nécessaires -->
   </div>
 </template>
 
 <script>
 import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader';
-definePageMeta({
-  layout: "utility"
-});
+
 export default {
   components: {
     QrcodeStream,
@@ -35,25 +32,5 @@ export default {
 </script>
 
 <style>
-/* Appliquer l'image d'arrière-plan à toute la page */
-.qr-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-image: url('/images/bg-qrcode.png'); /* Remplacez par le chemin de votre image */
-  background-size: cover;
-  background-position: center;
-}
-
-/* Zone de caméra de 300x300 pixels, centrée */
-.camera-box {
-  width: 300px;
-  height: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(255, 255, 255, 0.8); /* Optionnel : Ajouter un fond pour mettre en évidence la zone de la caméra */
-  border-radius: 8px; /* Optionnel : coins arrondis */
-}
+/* Ajoute le style nécessaire si besoin */
 </style>
