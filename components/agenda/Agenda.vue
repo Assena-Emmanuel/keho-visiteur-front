813108ddaf09ee61737941626da22a7e50059566
Assena-Emmanuel <assenaemmanuel3@gmail.com>
2024-10-25 13:33:13 +0000

    visiteur v2

--- components/agenda/Agenda.vue
+++ components/agenda/Agenda.vue
@@ -21,9 +21,17 @@ export default {
   },
   data() {
     return {
+        afficherModalNotification: true,
+        nomEmploye: 'KOUADIO Konan Jean',
+        visiteur: {
+          nom: 'Koffi Jean',
+          entreprise: 'KEHO GROUPE',
+          contact: '07 07 07 07 00'
+        },
         lang: 'fr',
-      calendarEvents,
-      calendarOptions: {
+
+        calendarEvents,
+        calendarOptions: {
         headerToolbar: {
           left: "prev,next today",
           center: "title",
@@ -193,6 +201,36 @@ export default {
 
 <template>
   <div>
+    <!-- Affiche modal notification -->
+    <BModal v-model="afficherModalNotification" title="Notification et envoi du visiteur" hide-footer>
+      <div class="d-flex justify-content-between pb-2">
+        <div style="height: 100%;">
+          <BImg thumbnail fluid src="/images/pages/personne.png" width="190" alt="Image 1" />
+        </div>
+        <div >
+          <div>
+            <p>Bonjour monsieur <strong>{{ nomEmploye }}</strong><br>Vous avez un visiteur en Salle</p>
+          </div>
+          <div>
+            <div class="d-flex justify-content-between line">
+              <span>Nom & Prénom</span>
+              <span>{{visiteur.nom}}</span>
+            </div>
+
+            <div class="d-flex justify-content-between line">
+              <span>Entreprise</span>
+              <span>{{ visiteur.entreprise }}</span>
+            </div>
+
+            <div class="d-flex justify-content-between line">
+              <span>Contact</span>
+              <span>{{ visiteur.contact }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BModal>
+
     <BRow no-body>
       <BCol cols="12">
         <BCard no-body>
@@ -277,5 +315,10 @@ export default {
     flex-wrap: wrap;
     gap: 4px;
   }
+}
+
+.line{
+  border-bottom: 1px solid #CCC;
+  margin-bottom: 10px;
 }
 </style>
--- components/dashboard/Visiteur.vue
+++ components/dashboard/Visiteur.vue
@@ -2,29 +2,29 @@
     <DashboardCommonStat />
     <div class="d-flex justify-content-end gap-5 mb-3">
         <button 
-            class="btn largeur rounded-0" 
-            :class="isActive === 'Jour' ? 'btn-primary' : 'btn-outline-secondary'" 
-            @click="setActive('Jour')">
-            Jour
-        </button>
-        <button 
-            class="btn largeur rounded-0" 
-            :class="isActive === 'Semaine' ? 'btn-primary' : 'btn-outline-secondary'" 
-            @click="setActive('Semaine')">
-            Semaine
-        </button>
-        <button 
-            class="btn largeur rounded-0" 
-            :class="isActive === 'Mois' ? 'btn-primary' : 'btn-outline-secondary'" 
-            @click="setActive('Mois')">
-            Mois
-        </button>
-        <button 
-            class="btn largeur rounded-0" 
-            :class="isActive === 'Année' ? 'btn-primary' : 'btn-outline-secondary'" 
-            @click="setActive('Année')">
-            Année
-        </button>
+                class="btn largeur" 
+                :class="isActive === 'Jour' ? 'btn-primary' : 'btn-inactive'" 
+                @click="setActive('Jour')">
+                Jour
+            </button>
+            <button 
+                class="btn largeur" 
+                :class="isActive === 'Semaine' ? 'btn-primary' : 'btn-inactive'" 
+                @click="setActive('Semaine')">
+                Semaine
+            </button>
+            <button 
+                class="btn largeur" 
+                :class="isActive === 'Mois' ? 'btn-primary' : 'btn-inactive'" 
+                @click="setActive('Mois')">
+                Mois
+            </button>
+            <button 
+                class="btn largeur" 
+                :class="isActive === 'Année' ? 'btn-primary' : 'btn-inactive'" 
+                @click="setActive('Année')">
+                Année
+            </button>
     </div>
     <div>
 
@@ -33,24 +33,21 @@
       <BCol cols="12">
         <BCard no-body>
           <BCardBody>
-            <BCardTitle>{{ title }}</BCardTitle>
-
-            <BRow class="mt-4">
-              <BCol sm="12" md="6" class="">
-                <div id="tickets-table_length" class="dataTables_length">
-                    <label class="d-inline-flex align-items-center">
-                      Afficher&nbsp;
-                      <BFormSelect class="border border-secondary" v-model="perPage" size="sm" :options="pageOptions"></BFormSelect>éléments&nbsp;
-                      
-                    </label>
-                  </div>
-              </BCol>
+            <BRow class="mb-3">
               <BCol sm="12" md="6">
-                <div id="tickets-table_filter" class="dataTables_filter text-md-end">
-                  <label class="d-inline-flex align-items-center">
-                    Recherche:
-                    <BFormInput v-model="filter" type="search" class="form-control border border-secondary form-control-sm ms-2"></BFormInput>
-                  </label>
+                <div class="">
+                    <span>Visites enregistrées</span>
+                </div>
+              </BCol>
+              <BCol sm="12" md="3">
+                <BFormSelect v-model="visiteurSelectionner" class="border border-secondary":options="listVisiteur" size="sm" />
+              </BCol>
+              <BCol sm="12" md="3">
+                <div class="input-group border border-secondary rounded-1">
+                    <span class="input-group-text">
+                        <i class="fas fa-search font-size-10"></i>
+                    </span>
+                    <BFormInput v-model="filter" type="search" id="input-small" size="sm"  placeholder="Rechercher..." />
                 </div>
               </BCol>
             </BRow>
@@ -91,10 +88,15 @@
               <BCol>
                 <div class="dataTables_paginate paging_simple_numbers d-flex justify-content-between">
                   <div id="tickets-table_length" class="dataTables_length">
-                    <label class="d-inline-flex align-items-center">
-                      Afficher de 1 à {{ perPage }} sur {{ totalRows }} éléments
-                      
-                    </label>
+                    <BCol sm="12" md="6" class="">
+                        <div id="tickets-table_length" class="dataTables_length">
+                            <label class="d-inline-flex align-items-center">
+                            Afficher&nbsp;
+                            <BFormSelect class="border border-secondary" v-model="perPage" size="sm" :options="pageOptions"></BFormSelect>éléments&nbsp;
+                            
+                            </label>
+                        </div>
+                    </BCol>
                   </div>
                   <ul class="pagination pagination-rounded mb-0">
                     <BPagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage" />
@@ -112,7 +114,15 @@
 export default {
     data() {
         return {
-       totalRows: 1,
+        isActive: 'Jour' ,
+        visiteurSelectionner: null,
+        listVisiteur : [
+        {value: null, text: 'Liste Visiteurs'},
+        {value: '1', text: 'AKA ANDRE'},
+        
+        ],
+
+        totalRows: 1,
         currentPage: 1,
         perPage: 5,
         pageOptions: [5, 10, 15, 20],
@@ -154,6 +164,10 @@ export default {
 .largeur{
     width: 90px;
     padding: 0;
-    align-items: center;
+}
+.btn-inactive {
+    border: 1px solid gray; /* Bordure grise */
+    background-color: white; /* Fond blanc */
+    color: gray; /* Couleur du texte */
 }
 </style>
