a0bba8fe200f988e1ec8db194431fb4af05fb327
Konate Ibrahima <ibrahim1155@outlook.com>
2025-02-05 07:21:56 +0000

    maj master

--- components/parametres/Utilisateur.vue
+++ components/parametres/Utilisateur.vue
@@ -1,9 +1,6 @@
 <script>
 import apiClient from "~/components/api/intercepteur";
-<<<<<<< HEAD
-=======
 import { useAuthStore } from "~/stores/auth.js";
->>>>>>> 1711b80159c1652ec637dd733d324dfc391af93a
 
 
 export default{
@@ -16,11 +13,7 @@ export default{
     data(){
         return{
         all_users: {},
-<<<<<<< HEAD
-        token: null,
-=======
         token: this.authStore.token,
->>>>>>> 1711b80159c1652ec637dd733d324dfc391af93a
         title: 'Liste des Utilisateurs',
         isEdit: false,
         isLoading: true,
@@ -36,11 +29,6 @@ export default{
     }
     },
     async mounted() {
-<<<<<<< HEAD
-        this.token = useCookie('token')
-        this.user = JSON.parse(localStorage.getItem("user"))
-=======
->>>>>>> 1711b80159c1652ec637dd733d324dfc391af93a
         await this.allUsers()
     },
     props:{
@@ -48,19 +36,10 @@ export default{
     },
 
     methods: {
-<<<<<<< HEAD
-        openModal(isEditMode, row=null, index = null, type) {
-            this.isEditMode = isEditMode;
-            this.selectedIndex = index;
-            this.selectedRow = row;
-            this.modal = true;
-            this.typeForme = type
-=======
         handleDataSelected(payload){
             this.selectedRow = payload.uuid
             this.modal = true
             this.isEditMode = !!payload.uuid;
->>>>>>> 1711b80159c1652ec637dd733d324dfc391af93a
         },
 
         openAddModal(){
@@ -89,24 +68,15 @@ export default{
 
             if (!response.data.error) {
                 this.data = response.data.data
-<<<<<<< HEAD
-            console.log("Liste des utilisateurs récupérée :", response.data);
-            }
-=======
-                console.log("---------------Liste des utilisateurs récupérée :", JSON.stringify(response.data));
             }
 
->>>>>>> 1711b80159c1652ec637dd733d324dfc391af93a
         } catch (error) {
             if (error.response && error.response.status === 401) {
             console.error("Erreur 401 : Jeton invalide ou utilisateur non authentifié.");
             } else {
             console.error("Erreur lors de la récupération des utilisateurs :", error);
             }
-<<<<<<< HEAD
-=======
 
->>>>>>> 1711b80159c1652ec637dd733d324dfc391af93a
         }finally {
             this.isLoading = false; // Fin du chargement
         }
@@ -126,15 +96,10 @@ export default{
         <FormsFormUser 
             :isOpen="modal"
             :isEditMode="isEditMode"
-<<<<<<< HEAD
-            :selectedIndex="selectedIndex" 
-            :data="selectedRow"
-=======
             :uuid="selectedRow"
             @update:isOpen="modal = $event"
             @update:uuid="modal = $event"
-            @close="selectedUserId = null"
->>>>>>> 1711b80159c1652ec637dd733d324dfc391af93a
+
         />
     </div>
 
@@ -144,13 +109,9 @@ export default{
         :data="data" 
         :title="title" 
         :show-addbtn="true" 
-        :typeForme="'utilisateur'" 
+        :typeForme="'user'" 
         :is-loading="isLoading"
-<<<<<<< HEAD
-        @edit="openModal(true, $event.row, $event.index)"
-=======
         @data-selected="handleDataSelected"
->>>>>>> 1711b80159c1652ec637dd733d324dfc391af93a
     />
 </div>
 </template>
