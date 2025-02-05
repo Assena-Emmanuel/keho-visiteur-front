79abc165f156f0f356f15ef7f9555e8911df7168
Konate Ibrahima <ibrahim1155@outlook.com>
2025-02-05 07:23:10 +0000

    maj master

--- stores/notified.js
+++ stores/notified.js
@@ -15,11 +15,7 @@ export const useNotifiedStore = defineStore("notified", {
         `${useRuntimeConfig().public.API_URL}/user/notifs`,
         {
           headers: {
-<<<<<<< HEAD
-            Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2tlaG8tdmlzaXRvcnMubG9jYWwvYXBpL2F1dGgvbG9naW4iLCJpYXQiOjE3MzYyNjA2MzQsImV4cCI6MTczNjM0NzAzNCwibmJmIjoxNzM2MjYwNjM0LCJqdGkiOiJTaTVIT2xZcmxXbW5qTDNMIiwic3ViIjoiNjQiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.NrRdPSX1o8W9qC03ehDmpWsRW-DvSSWixW02ebVns8w`, // Ajoutez le token Bearer ici
-=======
             Authorization: `Bearer ${authToken.token}`, // Ajoutez le token Bearer ici
->>>>>>> 1711b80159c1652ec637dd733d324dfc391af93a
             Accept: "application/json",
           },
         }
