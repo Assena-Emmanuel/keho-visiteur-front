3509cbe3cb4b80141c34d7e31a54c70e1fd07226
Konate Ibrahima <ibrahim1155@outlook.com>
2025-02-13 07:24:13 +0000

    recuperation

--- components/api/intercepteur.js
+++ components/api/intercepteur.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Configuration d'Axios
 const apiClient = axios.create({
-   baseURL: 'https://visitors.kehogroupe-ci.com/api', // Votre URL de base
- // baseURL:'https://keho-visitors.local/api',
+  // baseURL: 'https://visitors.kehogroupe-ci.com/api', // Votre URL de base
+  baseURL:'https://keho-visitors.local/api',
   headers: {
     'Content-Type': 'application/json',
   },
