eb18ceacd0989a5f1d8546b92e2191760fe4b3c8
Assena-Emmanuel <assenaemmanuel3@gmail.com>
2025-03-03 20:02:58 +0100

    maj

--- .nuxt/imports.d.ts
+++ .nuxt/imports.d.ts
@@ -43,5 +43,4 @@ export { flatUnwrap as unwrapSlot } from '../node_modules/@nuxtjs/mdc/dist/runti
 export { parseMarkdown } from '../node_modules/@nuxtjs/mdc/dist/runtime/parser';
 export { defineStore, acceptHMRUpdate, usePinia, storeToRefs } from '../node_modules/@pinia/nuxt/dist/runtime/composables';
 export { storages as piniaPluginPersistedstate } from '../node_modules/pinia-plugin-persistedstate/dist/nuxt/runtime/storages';
-export { useNuxtDevTools } from '../node_modules/@nuxt/devtools/dist/runtime/use-nuxt-devtools';
 export { definePageMeta } from '../node_modules/nuxt/dist/pages/runtime/composables';
--- .nuxt/types/content.d.ts
+++ .nuxt/types/content.d.ts
@@ -1,4 +1,4 @@
 declare module '#content/server' {
-  const serverQueryContent: typeof import('C:/Users/HP/OneDrive/Documents/JOB/keho-visiteur-front/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
-  const parseContent: typeof import('C:/Users/HP/OneDrive/Documents/JOB/keho-visiteur-front/node_modules/@nuxt/content/dist/runtime/server').parseContent
+  const serverQueryContent: typeof import('C:/Users/HP/OneDrive/Documents/keho-visiteur-front/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
+  const parseContent: typeof import('C:/Users/HP/OneDrive/Documents/keho-visiteur-front/node_modules/@nuxt/content/dist/runtime/server').parseContent
 }
