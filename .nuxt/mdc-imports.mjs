5004287ee3838018cff716da96ac2ffb0259a19a
Assena-Emmanuel <assenaemmanuel3@gmail.com>
2025-03-06 15:40:49 +0100

    maj avant de deployement sur versel

--- .nuxt/mdc-imports.mjs
+++ .nuxt/mdc-imports.mjs
@@ -1,7 +1,5 @@
-import _RemarkEmoji from 'remark-emoji'
 
 export const remarkPlugins = {
-  'remark-emoji': { instance: _RemarkEmoji },
 }
 
 export const rehypePlugins = {
---
+++ pages/reporting/index.vue
@@ -0,0 +1,23 @@
+<script>
+import "simplebar";
+
+export default {
+  data() {
+    return {
+      items: [
+        {
+          text: "Historique",
+          active: true
+        }
+      ]
+    };
+  },
+
+};
+</script>
+
+<template>
+  <SiteHeader :title="title" />
+  <PageHeader :title="title" :items="items" />
+  <Reporting />
+</template>
